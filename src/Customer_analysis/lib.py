9ceee9e9aa05e7493e3aa797073a136877bfa32a
Ahmed Alyafai <ahmedalyafai85@gmail.com>
2025-11-30 00:11:10 +0800

    Save notes

---
+++ src/Customer_analysis/lib.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import polars as pl
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+from pathlib import Path
+import polars.selectors as cs
+
+class loadProfile:
+    def __init__(self,data):
+        self.data=data
+        self.devices=[n for n in data.columns if n.startswith('out.electricity')]
+        self.dev_count=len(self.devices)
+        plt.style.use('ggplot')
+        
+    def boxplot_exp(self):
+        fig, ax=plt.subplots(self.dev_count, 1, figsize=(10, 7 * self.dev_count))
+        for i,x in enumerate(self.devices):
+            sns.boxplot(data=self.data, x=x, ax=ax[i])
+    
+    def hist_exp(self):
+        fig, ax=plt.subplots(self.dev_count, 1, figsize=(10, 7 * self.dev_count))
+        for i,x in enumerate(self.devices):
+            sns.histplot(data=self.data, x=x, ax=ax[i])
+                
+    def boxpen_exp(self):
+        fig, ax=plt.subplots(self.dev_count, 1, figsize=(10, 7 * self.dev_count))
+        for i,x in enumerate(self.devices):
+            sns.boxenplot(data=self.data, x=x, ax=ax[i])
